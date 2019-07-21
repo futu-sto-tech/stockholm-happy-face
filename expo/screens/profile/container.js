160301845e4b711532af8f8cf927c36ffb4e5e89
Robin Andeer <robin.andeer@gmail.com>
2019-07-21 22:50:48 +0200

    Convert class component to use hooks

--- expo/screens/profile/container.js
+++ expo/screens/profile/container.js
@@ -1,55 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavigationEvents } from 'react-navigation'
 import * as Haptics from 'expo-haptics'
 
 import backend from '../../lib/backend'
 import ProfileScreen from './screen'
 
-class ProfileContainer extends React.Component {
-  state = { user: null, entries: [], currentEntry: null, isDeleting: false }
+const ProfileContainer = ({ navigation }) => {
+  const [entries, setEntries] = useState([])
+  const [currentEntry, setCurrentEntry] = useState(null)
+  const [isDeleting, setIsDeleting] = useState(false)
 
-  handlePressInput = () => {
-    const user = this.props.navigation.getParam('user')
-    this.props.navigation.navigate('NewEntry', { user })
+  const handlePressInput = () => {
+    const user = navigation.getParam('user')
+    if (user) navigation.navigate('NewEntry', { user })
   }
 
-  handlePressEntry = async entry => {
-    this.props.navigation.navigate('Entry', { entry })
+  const handlePressEntry = async entry => {
+    navigation.navigate('Entry', { entry })
     await Haptics.selectionAsync()
   }
 
-  handlePressDeleteCurrentEntry = async () => {
-    this.setState({ isDeleting: true })
-    await backend.deleteEntry(this.state.currentEntry)
-    const user = this.props.navigation.getParam('user')
-    const userEntryProps = await backend.getUserEntries(user.id)
-    this.setState({ ...userEntryProps, isDeleting: false })
-  }
-
-  handleWillFocus = async payload => {
-    if (payload.action.type !== 'Navigation/BACK') {
-      const user = this.props.navigation.getParam('user')
-      const userEntryProps = await backend.getUserEntries(user.id)
-      this.setState({ ...userEntryProps })
+  const updateEntries = async () => {
+    const user = navigation.getParam('user')
+    if (user) {
+      const userData = await backend.getUserEntries(user.id)
+      setEntries(userData.entries)
+      setCurrentEntry(userData.currentEntry)
     }
   }
 
-  render() {
-    return (
-      <>
-        <NavigationEvents onWillFocus={this.handleWillFocus} />
-        <ProfileScreen
-          user={this.state.user}
-          currentEntry={this.state.currentEntry}
-          entries={this.state.entries}
-          onPressInput={this.handlePressInput}
-          onPressEntry={this.handlePressEntry}
-          onPressDeleteCurrentEntry={this.handlePressDeleteCurrentEntry}
-          isDeleting={this.state.isDeleting}
-        />
-      </>
-    )
+  const handlePressDeleteCurrentEntry = async () => {
+    setIsDeleting(true)
+    await backend.deleteEntry(currentEntry)
+    setIsDeleting(false)
+    await updateEntries()
   }
+
+  const handleWillFocus = async payload => {
+    if (payload.action.type !== 'Navigation/BACK') {
+      await updateEntries()
+    }
+  }
+
+  return (
+    <>
+      <NavigationEvents onWillFocus={handleWillFocus} />
+      <ProfileScreen
+        currentEntry={currentEntry}
+        entries={entries}
+        onPressInput={handlePressInput}
+        onPressEntry={handlePressEntry}
+        onPressDeleteCurrentEntry={handlePressDeleteCurrentEntry}
+        isDeleting={isDeleting}
+      />
+    </>
+  )
 }
 
 export default ProfileContainer
