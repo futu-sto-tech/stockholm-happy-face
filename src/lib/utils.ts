ce1f6c11a27c345614ddc56477b4142ca72c215c
Robin Andeer <robin.andeer@gmail.com>
2020-09-25 11:21:59 +0200

    Fix type errors in hexToHSL function
    Gotta love JS behavior...

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -55,18 +55,15 @@ export function hexToHSL(hexColor: string, hueModifier: number, saturationModifi
   // Convert hex to RGB first
   let r = 0, g = 0, b = 0;
   if (hexColor.length == 4) {
-    r = "0x" + hexColor[1] + hexColor[1];
-    g = "0x" + hexColor[2] + hexColor[2];
-    b = "0x" + hexColor[3] + hexColor[3];
+    r = ("0x" + hexColor[1] + hexColor[1]) / 255;
+    g = ("0x" + hexColor[2] + hexColor[2]) / 255;
+    b = ("0x" + hexColor[3] + hexColor[3]) / 255;
   } else if (hexColor.length == 7) {
-    r = "0x" + hexColor[1] + hexColor[2];
-    g = "0x" + hexColor[3] + hexColor[4];
-    b = "0x" + hexColor[5] + hexColor[6];
+    r = ("0x" + hexColor[1] + hexColor[2]) / 255;
+    g = ("0x" + hexColor[3] + hexColor[4]) / 255;
+    b = ("0x" + hexColor[5] + hexColor[6]) / 255;
   }
   // Then to HSL
-  r /= 255;
-  g /= 255;
-  b /= 255;
   let cmin = Math.min(r, g, b),
       cmax = Math.max(r, g, b),
       delta = cmax - cmin,
